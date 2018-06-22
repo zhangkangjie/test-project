440466fcbe27861662a7f3a21ba850e1bebc74fa
zhangkangjie <825272470@qq.com>
2018-06-22 18:41:50 +0800

    update

--- demo/src/main/java/java8/StreamTest.java
+++ demo/src/main/java/java8/StreamTest.java
@@ -2,7 +2,9 @@ package java8;
 
 import com.zkj.java8.Person;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -39,6 +41,25 @@ public class StreamTest {
         Map<String, Person> companyMap = list.stream().collect(Collectors.toMap(Person::getFirstName, person -> person));
         Map<String, List<Person>> collect2 = list2.stream().collect(Collectors.groupingBy(Person::getFirstName));
         System.out.println(collect2);
+
+        //sorted
+        System.out.println("before sort list 2");
+        list2.stream().forEach(person -> System.out.print(person.getAge()+" "));
+        System.out.println();
+        System.out.println("after sorted");
+        list2.stream().sorted(Comparator.comparing(Person::getAge)).forEach(person -> System.out.print(person.getAge()+" "));
+        System.out.println();
+        System.out.println("reverse order");
+        list2.stream().sorted(Comparator.comparing(Person::getAge).reversed()).forEach(person -> System.out.print(person.getAge()+" "));
+        System.out.println();
+
+        //min or max
+        Person minPerson = list2.stream().min(Comparator.comparing(Person::getAge)).get();
+        System.out.println("min age "+minPerson.getAge());
+        //remove min person
+        List<Person> list22 = new ArrayList<>(list2);
+        list22.remove(minPerson);
+        System.out.println(list22);
 
 
     }
