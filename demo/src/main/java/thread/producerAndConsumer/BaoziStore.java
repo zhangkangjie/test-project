7ab80f072573e8f4813d4819b1a16757df56ff52
zhangkangjie <825272470@qq.com>
2019-02-20 17:58:29 +0800

    更新

--- demo/src/main/java/thread/producerAndConsumer/BaoziStore.java
+++ demo/src/main/java/thread/producerAndConsumer/BaoziStore.java
@@ -14,6 +14,7 @@ import java.util.function.Supplier;
  * 包子铺
  */
 public class BaoziStore {
+    private int catacity = 5;
     private Queue<Baozi> repository = new ArrayBlockingQueue<>(10);
 
     //private Queue<Consumer> consumers = new ArrayBlockingQueue<>(20);
@@ -24,70 +25,71 @@ public class BaoziStore {
     private Supplier<Baozi> baoziSupplier = Baozi::new;
 
     private Object consumerLockObject = new Object();
+    private Object productLockObject = new Object();
 
     private Thread productThread = new Thread(() -> {
-        //获取生产者对象锁
-        synchronized (this) {
-            while (true) {
-                try {
-                    //x秒一个
-                    Thread.sleep(500);
-                    Baozi baozi = baoziSupplier.get();
-                    boolean b = repository.offer(baozi);
-                    synchronized (consumerLockObject){
-                        consumerLockObject.notify();
-                    }
-                    if (!b) {
-                        Printer.println("包子满了");
-                        //生产者wait,并释放生产者对象锁
-                        this.wait();
-                    } else {
-                        Printer.println("做了个包子" + baozi);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+
+        make();
 
     }, "生产者");
 
-//    /**
-//     * 做包子
-//     */
-//    private void make(){
-//
-//        Baozi baozi = baoziSupplier.get();
-//        boolean b = repository.offer(baozi);
-//        if (!b){
-//            System.out.println("包子装满了");
-//        }else {
-//            System.out.println("装个包子"+baozi);
-//        }
-//    }
+    private void make(){
+        while (true) {
+            try {
+                //x秒一个
+                Thread.sleep(500);
+                synchronized (productLockObject) {
+                    //System.out.println(this);
+                    while (repository.size() == catacity){
+                        Printer.println("包子满了");
+                        productLockObject.wait();
+                    }
+                    Baozi baozi = baoziSupplier.get();
+                    repository.offer(baozi);
+                    Printer.println("做了个包子" + baozi);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private Thread seller = new Thread(()->{
+       while(true){
+           synchronized (consumerLockObject){
+               if (!repository.isEmpty()){
+                   consumerLockObject.notify();
+               }
+           }
+           synchronized (productLockObject) {
+               if (repository.size() != catacity){
+                   productLockObject.notify();
+               }
+           }
+           try {
+               Thread.sleep(100L);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+    },"seller");
+
+
 
 
 
     /**
      * 卖包子
      */
-    public void sell(Consumer<Baozi> consumer) {
+    public void sell(Consumer<Baozi> consumer) throws InterruptedException {
         synchronized (consumerLockObject) {
-            Baozi baozi = repository.poll();
-            if (baozi == null) {
-                Printer.println("没包子了，快做");
-                //TODO 死锁
-                //等着有包子
-                synchronized (productThread) {
-                    productThread.notify();
-                }
-                try {
-                    consumerLockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                baozi = repository.poll();
+            while (repository.isEmpty()){
+                Printer.println("没包子了,稍等");
+                consumerLockObject.wait();
             }
+            Baozi baozi = repository.poll();
             consumer.accept(baozi);
         }
 
@@ -100,6 +102,7 @@ public class BaoziStore {
         Printer.println("---店铺开张---");
         //开始有人做包子
         this.productThread.start();
+        this.seller.start();
 
     }
 
@@ -131,7 +134,8 @@ class Setup {
                 for (int i = 0; i < 50; i++) {
                     Thread.sleep(RandomUtils.nextInt(3) * 1000 + 1);
                     String consumerIndex = String.valueOf(i+1);
-                    baoziStore.sell(baozi -> Printer.println("消费者" + consumerIndex + "买了包子" + baozi));
+                    Printer.println("消费者" + consumerIndex + "去买包子" );
+                    baoziStore.sell(baozi -> Printer.println("消费者" + consumerIndex + "买到了包子" + baozi));
                 }
                 Printer.println("没人了");
             } catch (InterruptedException e) {
