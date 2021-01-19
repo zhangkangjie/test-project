0de78fd152c99d9f1ddcbbc37187d2686bebf8cb
kangjie.zhang <kangjie0603@163.com>
2021-01-19 10:47:10 +0800

    update pattern mather demo

--- demo/src/main/java/PatternMatherTest.java
+++ demo/src/main/java/PatternMatherTest.java
@@ -2,23 +2,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatternMatherTest {
-    private final static Pattern decorateStoreUrlPattern = Pattern.compile("369\\.com/(\\d+)/index\\.html");
+
     public static void main(String[] args) {
-        String url = "http://www.i369.com/s/30.html";
-        url ="http://www.i369.com/63/index.html  i369.com/80/index.html?xxx=x ";
+        Pattern pattern = Pattern.compile(":(\\w+)");
+        Matcher matcher = pattern.matcher("this a static :name :name :value :value");
 
-        Matcher matcher = decorateStoreUrlPattern.matcher(url);
         while (matcher.find()) {
-            String group = matcher.group();
-            System.out.println(group);
-            int i = matcher.groupCount();
-            for (int j = 0; j <= i; j++) {
-                System.out.println(j+" - "+matcher.group(j));
-            }
+            System.out.print(matcher.group() + " " + matcher.group(1));
+            System.out.print(matcher.start() + "|" + matcher.end() + " ");
+
         }
-        System.out.println("------------------");
-
-
-        System.out.println("http://111.1".matches("http://\\w*\\.\\w*"));
     }
 }
